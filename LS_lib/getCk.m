a8879e591c647afed21b1c45ed9d7c48eecb5cd3
Marco Costanzo <marco.costanzo@unicampania.it>
2018-11-27 14:52:00 +0100

    Mods

--- LS_lib/getCk.m
+++ LS_lib/getCk.m
@@ -1,6 +1,6 @@
 function C_k = getCk( k )
 
-C_k = (3/2)*k*(gamma(3/k))/( gamma(1/k) * gamma(2/k) );
+C_k = (3/2)*k.*(gamma(3./k))./( gamma(1./k) .* gamma(2./k) );
 
 %C_k = k / (2* beta(2/k , 1/k + 1) );
 
